deba0c17e59a974e32068ebb3b0f5e149acd9776
Fernando Zuleta <crop94@gmail.com>
2024-08-05 20:18:15 -0400

    Cambio - Secciones finales concretadas de acuerdo a maqueta

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,29 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-$("input").focus(function () {
-    $(this).css("background-color", "#e7e9eb");
-});
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
 
-$("#enviarCorreo").click(function () {
-    alert("¡El correo fue enviado correctamente!");
-});
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Mensajes enviado con éxito !!!', 'success')
+  })
+}
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
