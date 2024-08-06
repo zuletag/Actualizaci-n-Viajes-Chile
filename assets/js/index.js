13f266bb60d105526ae10c560b7656ac82124e95
Fernando Zuleta <crop94@gmail.com>
2024-08-05 21:00:40 -0400

    Fix - Index y JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+//* import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js' *//
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
